a6b553f3cbcd0aefd688e5883244e6c8604c1fa9
Gabriel Paiva <gabigolpaiva@gmail.com>
2025-08-20 17:28:19 -0300

    ❇️  feat: Adaptar a captura dos dados para o novo circuito

--- dados_dos_sensores/dados_dos_sensores.ino
+++ dados_dos_sensores/dados_dos_sensores.ino
@@ -1,21 +1,21 @@
 #include <MPU6050_tockn.h>
 #include <Wire.h>
 
-#define POLEGAR A0
+#define POLEGAR 13
 #define INDICADOR A1
 #define MEDIO A2
 #define ANELAR A3
-#define MINIMO A4
+#define MINIMO A0
 
 // VALORES DE CALIBRAÇÃO
 
 #define X0P 520
-#define X0I 530
+#define X0I 500
 #define X0M 540
-#define X0A 515
+#define X0A 520
 #define X0m 600
 #define XpiP 403
-#define XpiI 420
+#define XpiI 430
 #define XpiM 365
 #define XpiA 371
 #define Xpim 349
@@ -44,7 +44,7 @@ void setup()
 void loop()
 {
   mpu6050.update();
-  polegar = analogRead(POLEGAR);
+  polegar = digitalRead(POLEGAR);
   indicador = analogRead(INDICADOR);
   medio = analogRead(MEDIO);
   anelar = analogRead(ANELAR);
@@ -52,8 +52,7 @@ void loop()
   Serial.print(String(int(mpu6050.getAngleX())) + ",");
   Serial.print(String(int(mpu6050.getAngleY())) + ",");
   Serial.print(String(int(mpu6050.getAngleZ())) + ",");
-  //Serial.print("0,0,0,");
-  Serial.println(angulo(polegar, X0P, XpiP) + "," + angulo(indicador, X0I, XpiI) + "," + angulo(medio, X0M, XpiM) + "," + angulo(anelar, X0A, XpiA) + ",0");// + angulo(minimo, X0m, Xpim));
+  Serial.println(String(polegar) + "," + angulo(indicador, X0I, XpiI) + "," + angulo(medio, X0M, XpiM) + "," + angulo(anelar, X0A, XpiA) + "," + angulo(minimo, X0m, Xpim));
   //Serial.println(String(polegar) + "," + String(indicador) + "," + String(medio) + "," + String(anelar) + "," + String(minimo));
   delay(50);
 }
