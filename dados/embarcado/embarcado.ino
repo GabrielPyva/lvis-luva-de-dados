cb8ef3a0a9755171ef0ccad1c5a114cb733285d4
Gabriel Paiva <gabigolpaiva@gmail.com>
2025-10-12 16:15:36 -0300

    ♻️ refactor: Atualizar sub-rotina imprime
    squash de dois commits

--- dados/embarcado/embarcado.ino
+++ dados/embarcado/embarcado.ino
@@ -22,22 +22,22 @@ MPU6050 mpu6050(Wire);
 Dedo dedo[5];
 
 int ajuste(int, int, int);
-void imprime(char);
+void imprime(char='v');
 void le_dedos();
 void calcula_angulos_dos_dedos();
 void calibra_dedos(int = 100, int = 8000);
 
 void setup()
 {
-  dedo[0].porta = 3;  dedo[0].nome = "polegar"; dedo[0].aberto = XaP; dedo[0].fechado = XfP;
+  dedo[0].porta = 3;  dedo[0].nome = "polegar";   dedo[0].aberto = XaP; dedo[0].fechado = XfP;
   dedo[1].porta = A0; dedo[1].nome = "indicador"; dedo[1].aberto = XaI; dedo[1].fechado = XfI;
-  dedo[2].porta = A1; dedo[2].nome = "medio"; dedo[2].aberto = XaM; dedo[2].fechado = XfM;
-  dedo[3].porta = A2; dedo[3].nome = "anelar"; dedo[3].aberto = XaA; dedo[3].fechado = XfA;
-  dedo[4].porta = A3; dedo[4].nome = "minimo"; dedo[4].aberto = Xam; dedo[4].fechado = Xfm;
+  dedo[2].porta = A1; dedo[2].nome = "medio";     dedo[2].aberto = XaM; dedo[2].fechado = XfM;
+  dedo[3].porta = A2; dedo[3].nome = "anelar";    dedo[3].aberto = XaA; dedo[3].fechado = XfA;
+  dedo[4].porta = A3; dedo[4].nome = "minimo";    dedo[4].aberto = Xam; dedo[4].fechado = Xfm;
   for (int i = 0; i < 5; i++) pinMode(dedo[i].porta, INPUT);
-  mpu6050.calcGyroOffsets(true);
   Serial.begin(9600);
   mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
   Wire.begin();
   //calibra_dedos();
 }
@@ -47,7 +47,7 @@ void loop()
   mpu6050.update();
   le_dedos();
   calcula_angulos_dos_dedos();
-  imprime('v');
+  imprime('c');
   delay(100);
 }
 
@@ -59,12 +59,25 @@ void le_dedos()
 
 void calcula_angulos_dos_dedos()
 {
-  for (int i = 0; i < 5; i++) dedo[i].angulo = ajuste(dedo[i].leitura, dedo[i].fechado, dedo[i].aberto);
+  for (int i = 0; i < 5; i++)
+    dedo[i].angulo = ajuste(dedo[i].leitura, dedo[i].fechado, dedo[i].aberto);
 }
 
 int ajuste(int bits, int xf, int xa)
 {
   return 90 * (bits - xa)/(xf - xa);
+}
+
+void imprime(char modo)
+{
+  Serial.print(mpu6050.getAngleX(), 0); Serial.print(',');
+  Serial.print(mpu6050.getAngleY(), 0); Serial.print(',');
+  Serial.print(mpu6050.getAngleZ(), 0); Serial.print(',');
+  for (int i = 0; i < 5; i++)
+  {
+    Serial.print(modo == 'v' ? dedo[i].angulo : dedo[i].leitura);
+    Serial.print(i != 4 ? ',' : '\n');
+  }
 }
 
 void calibra_dedos(int amostras, int espera)
@@ -75,7 +88,8 @@ void calibra_dedos(int amostras, int espera)
   for (int t = 0; t < amostras; t++)
   {
     le_dedos();
-    for (int i = 0; i < 5; i++) dedo[i].fechado += dedo[i].leitura / amostras;
+    for (int i = 0; i < 5; i++)
+      dedo[i].fechado += dedo[i].leitura / amostras;
     delay(50);
   }
   Serial.println("ABRE");
@@ -84,34 +98,8 @@ void calibra_dedos(int amostras, int espera)
   for (int t = 0; t < amostras; t++)
   {
     le_dedos();
-    for (int i = 0; i < 5; i++) dedo[i].aberto += dedo[i].leitura / amostras;
+    for (int i = 0; i < 5; i++)
+      dedo[i].aberto += dedo[i].leitura / amostras;
     delay(50);
-  }
-}
-
-void imprime(char modo)
-{
-  char buffer[5];
-  switch (modo) {
-    case 'v':
-      Serial.print(String(int(mpu6050.getAngleX())) + ',');
-      Serial.print(String(int(mpu6050.getAngleY())) + ',');
-      Serial.print(String(int(mpu6050.getAngleZ())) + ',');
-      for (int i = 0; i < 5; i++)
-      {
-        Serial.print(String(dedo[i].angulo));
-        Serial.print(i != 4 ? ',' : '\n');
-      }
-      break;
-    case 'c':
-      Serial.print(String(int(mpu6050.getAngleX())) + ',');
-      Serial.print(String(int(mpu6050.getAngleY())) + ',');
-      Serial.print(String(int(mpu6050.getAngleZ())) + ',');
-      for (int i = 0; i < 5; i++)
-      {
-        Serial.print(String(dedo[0].leitura));
-        Serial.print(i != 4 ? ',' : '\n');
-      }
-      break;
   }
 }
